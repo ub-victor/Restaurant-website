93f6adcf7d23473724b7beee0de6fe061b277bab
ub-victor <victoireushindi371@gmail.com>
2025-01-29 12:37:58 +0200

    Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */  
+module.exports = {  
+  content: ['./index.html'], // Make sure this points to your HTML file  
+  theme: {  
+    extend: {},  
+  },  
+  plugins: [],  
+};
